42fa537a4b7788d3e31859f32911fc064c55d678
Nicky <nshanker412@gmail.com>
2024-06-12 21:55:25 -0400

    Initial Commit

---
+++ GetBetter./ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GetBetter.
+//
+//  Created by Nicky Shanker on 6/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GetBetter./GetBetter_App.swift
@@ -0,0 +1,17 @@
+//
+//  GetBetter_App.swift
+//  GetBetter.
+//
+//  Created by Nicky Shanker on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GetBetter_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
